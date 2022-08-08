347c2ee2bc40817c17c331b71b4c449b68e61213
malubrandaor <malubrandaor@gmail.com>
2022-08-08 17:38:25 -0300

    pd

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,9 +34,9 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
-    expect(typeof productDetails).toBe('function');
+    expect(productDetails).toBe('function');
     // Teste se o retorno da função é um array.
-    expect(typeof productDetails('Máscara', 'Alcool gel')).toBe('object');
+    expect(productDetails('Máscara', 'Alcool gel')).toBe('object');
     // Teste se o array retornado pela função contém dois itens dentro.
     expect(productDetails('Máscara', 'Alcool gel')).toHaveLength(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
